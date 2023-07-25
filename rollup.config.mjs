1630b4c91a663f4ac28cbb293734716cd51e8ca9
inotom <wdf7322@yahoo.co.jp>
2023-07-25 13:48:11 +0900

    Upgrade npm packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,17 @@
 /** @prettier */
 
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import html2 from 'rollup-plugin-html2';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 import camelCase from 'lodash.camelcase';
 import upperFirst from 'lodash.upperfirst';
 
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 // Remove npm package scope.
 const unscopedName = pkg.name.replace(/^@.*\//, '');
@@ -21,16 +21,13 @@ const moduleName = upperFirst(camelCase(unscopedName));
 const banner = `/*! ${unscopedName}.js v${pkg.version} ${pkg.author} | ${pkg.license} License */`;
 
 const plugins = [
-  resolve(),
+  nodeResolve(),
   // `check: false` for fixing (plugin rpt2) Error: Could not find source file lit-element/lib/updating-element.js
   // https://github.com/ezolenko/rollup-plugin-typescript2/issues/214
   typescript({ include: '**/*.{ts,js}', check: false }),
   commonjs({ extensions: ['.ts', '.js'] }),
 ];
-let pluginsBrowser = [
-  ...plugins,
-  html2({ template: 'src/html/index.html' }),
-];
+let pluginsBrowser = [...plugins, html2({ template: 'src/html/index.html' })];
 
 if (process.env.NODE_ENV === 'development') {
   pluginsBrowser = [
